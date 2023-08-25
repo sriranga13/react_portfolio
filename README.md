cecf526ff58519bff7aef7308a06e9eff67b85b1
sriranga13 <80486964+sriranga13@users.noreply.github.com>
2023-08-25 16:54:13 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,16 @@
+### Description
+
+A simple portfolio template for developer/designers built with React. 
+
+### [live preview](https://ubaimutl.github.io/react-portfolio/)
+
+[![react portfoiio](src/assets/images/react%20portfolio%20gif.gif)](https://ubaimutl.github.io/react-portfolio/)
+
+### Features
+
+- Fully Responsive
+- Multi-Page Layout
+- Contact Form With EmailJs
+- React-Bootstrap
+- Edit Content From One Place
+
---
+++ src/content_option.js
@@ -0,0 +1,144 @@
+const logotext = "Sri";
+const meta = {
+    title: "Sri Gorantla",
+    description: "I’m Sri Gorantla Software devloper,currently working in Cary,NC",
+};
+
+const introdata = {
+    title: "I’m Sri Gorantla",
+    animated: {
+        first: "I love coding",
+        second:"I build ML models",
+        third: "I code cool websites",
+        fourth: "I develop mobile apps",
+    },
+    description: "Passionate front-end developer with a knack for crafting engaging user interfaces.",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "A bit about myself",
+    aboutme: "I'm a Seasoned software developer specializing in front-end excellence, with a well-rounded foundation in full-stack development and insights from the world of data science.. My goal is to combine my diverse skills to create innovative and impactful solutions that bridge the gap between technology and user needs.",
+};
+const worktimeline = [{
+        jobtitle: "Front End Developer",
+        where: "Q9Software",
+        date: "2022",
+    },
+    {
+        jobtitle: "Teaching Assistant",
+        where: "UAkron",
+        date: "2021",
+    },
+    {
+        jobtitle: "Full Stack Developer",
+        where: "FrontRow",
+        date: "2019",
+    },
+    {
+        jobtitle: "Web Developer",
+        where: "BSNL",
+        date: "2018",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "Djano",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 70,
+    },
+    {
+        name: "Jquery",
+        value: 85,
+    },
+    {
+        name: "Java",
+        value: 90,
+    },
+    {
+        name: "Spring",
+        value: 80,
+
+    },
+];
+
+const services = [
+    {
+        title: "Front-End Development",
+        description: "Crafting visually stunning and highly interactive user interfaces that engage and delight users, backed by my 2 years of experience in front-end development.",
+    },
+    {
+        title: "Full-Stack Development",
+        description: "Bringing a comprehensive perspective to software development by seamlessly handling both front-end and back-end tasks for end-to-end solutions.",
+    },
+    {
+        title: "Data Science Insights",
+        description: "Leveraging data science expertise to extract valuable insights from complex datasets, enabling data-driven decision-making and innovation.",
+    },
+];
+
+
+const dataportfolio = [
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "A three dimensional word puzzle developed using javascript and jquery.",
+        demolink: "https://sriranga13.github.io/wordify/",
+        codelink: "https://github.com/sriranga13/wordify"
+    },
+
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "An application that is trained using ML models and uses computer vision to recognize the facial expression with 97% accuracy in real time.",
+        codelink:"https://github.com/sriranga13/Facial-Expression-Detection",
+    },
+
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "A Deep Learning application that uses computer vision to detect and locate multiple keypoints for a given image or a video.",
+        codelink: "https://github.com/sriranga13/Multi-Person-Key-Point-Detection",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "An interactive 3D robot",
+        demolink: "https://sriranga13.github.io/My-3D-Robot/",
+        codelink:"https://github.com/sriranga13/My-3D-Robot"
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "srichow0987@gmail.com",
+    YOUR_FONE: "(330)671-1989",
+    description: "Available for amazing projects!Contact me and let's talk. ",
+    YOUR_SERVICE_ID: "service_lcb2r9f",
+    YOUR_TEMPLATE_ID: "template_ynbmecw",
+    YOUR_PUBLIC_KEY: "3T9fbCDdz95D892Ld",
+};
+
+const socialprofils = {
+    github: "https://github.com/sriranga13",
+    linkedin: "https://https://www.linkedin.com/in/sri-gorantla-a9121b204/",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
